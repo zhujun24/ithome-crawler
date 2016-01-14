ca49557f2ea1e8dea998e5c94c6c0b03479f9aee
zhujun24 <zhujun87654321@gmail.com>
2016-01-14 12:24:29 +0800

    queue request

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var co = require('co');
 var cheerio = require('cheerio');
-var iconv = require('iconv-lite');
-var http = require('http');
-var BufferHelper = require('bufferhelper');
-var async = require('async');
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
 
 var aidExp = /\/\d+/;
 var commentExp = /unescape\('.*/;
@@ -16,133 +15,83 @@ var done = [{
 
 var loadData = function (url) {
   return new Promise(function (resolve, reject) {
-    var req = http.request(url, function (res) {
-      var bufferHelper = new BufferHelper();
-      res.on('data', function (chunk) {
-        bufferHelper.concat(chunk);
-      });
-      res.on('end', function () {
-        var bufferhelper = bufferHelper.toBuffer();
-        resolve(iconv.decode(bufferhelper, 'GBK'));
-      });
+    request(url, {timeout: 5000}, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body);
+      } else {
+        var result = {
+          error: error,
+          statusCode: response.statusCode
+        };
+        reject(result);
+      }
     });
-
-    req.on('error', function (e) {
-      reject(e.message);
-      console.log('loadData error: ' + e.message);
-    });
-    req.end();
   });
 };
 
-loadData('http://www.ithome.com/list/list_9999.html').then(function (data) {
-  var $ = cheerio.load(data, {
-    normalizeWhitespace: true,
-    //xmlMode: true,
-    decodeEntities: false
-  });
-  var pageAccount = parseInt($('.pagenew>span.current').text(), 10);
-  var gen1 = function*() {
-    for (var i = 0; i < pageAccount; i++) {
+var pageAccount = 10;
+var gen1 = function*() {
+  for (var i = 0; i < pageAccount; i++) {
+    console.log('request page ' + (i + 1) + ' start!============');
+    try {
       var result = yield loadData('http://www.ithome.com/list/list_' + (i + 1) + '.html');
-      var $ = cheerio.load(result, {
-        normalizeWhitespace: true,
-        //xmlMode: true,
-        decodeEntities: false
-      });
+      console.log('request page ' + (i + 1) + ' success!');
+      var $ = cheerio.load(result);
       $('.ulcl>li:not(.hr)>a').each(function (index, element) {
         var aid = $(element).attr('href').match(aidExp)[0].substr(1);
         aidArr.push(aid);
       });
+    } catch (error) {
+      console.log(error);
     }
-  };
+  }
+};
 
-  co(gen1).then(function () {
-    var gen2 = function*() {
-      console.log(aidArr);
-
-      var concurrencyCount = 0;
-      var fetchUrl = function (url, callback) {
-        concurrencyCount++;
-        console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url);
-        loadData('http://www.ithome.com/ithome/CommentCount.aspx?newsid=' + url).then(function (res) {
-          concurrencyCount--;
-
-            var hehe = res.match(commentExp)[0].substr(10);
-            var html = hehe.substring(0, hehe.length - 2);
-            var $ = cheerio.load(unescape(html));
-            $('#ulcommentlist>li').each(function (index, element) {
-              var hehehehe = $($(element).find('div.info.rmp')[0]).find('.mobile');
-              if (hehehehe.length) {
-                //var name = $(hehehehe).find('a').text();
-                var type = $(hehehehe).attr('class').substr(7);
-                var isExist = true;
-                $(done).each(function (index2, element2) {
-                  if (element2.type === type) {
-                    element2.sum++;
-                    isExist = false;
-                    return false;
-                  }
-                });
-                if (isExist) {
-                  done.push({
-                    type: type,
-                    sum: 1
-                  });
-                }
-              } else {
-                done[0].sum++;
+co(gen1).then(function () {
+  var gen2 = function*() {
+    for (var i = 0; i < aidArr.length; i++) {
+      var url = 'http://www.ithome.com/ithome/CommentCount.aspx?newsid=' + aidArr[i];
+      console.log('request: ' + aidArr[i] + ' start!============');
+      try {
+        var result = yield loadData(url);
+        console.log('request: ' + aidArr[i] + ' success!');
+        var hehe = result.match(commentExp)[0].substr(10);
+        var html = hehe.substring(0, hehe.length - 2);
+        var $ = cheerio.load(unescape(html));
+        $('#ulcommentlist>li').each(function (index, element) {
+          var hehehehe = $($(element).find('div.info.rmp')[0]).find('.mobile');
+          if (hehehehe.length) {
+            //var name = $(hehehehe).find('a').text();
+            var type = $(hehehehe).attr('class').substr(7);
+            var isExist = true;
+            $(done).each(function (index2, element2) {
+              if (element2.type === type) {
+                element2.sum++;
+                isExist = false;
+                return false;
               }
             });
-
-          callback(null, url);
+            if (isExist) {
+              done.push({
+                type: type,
+                sum: 1
+              });
+            }
+          } else {
+            done[0].sum++;
+          }
         });
-      };
-
-      async.mapLimit(aidArr, 10, function (url, callback) {
-        fetchUrl(url, callback);
-      }, function (err, result) {
-        console.log('final:');
-        for (var i = 0; i < result.length; i++) {
-          console.log(result[i]);
-        }
-      });
-      //for (var i = 0; i < aidArr.length; i++) {
-      //  var result = yield loadData('http://www.ithome.com/ithome/CommentCount.aspx?newsid=' + aidArr[i]);
-      //  var hehe = result.match(commentExp)[0].substr(10);
-      //  var html = hehe.substring(0, hehe.length - 2);
-      //  var $ = cheerio.load(unescape(html));
-      //  $('#ulcommentlist>li').each(function (index, element) {
-      //    var hehehehe = $($(element).find('div.info.rmp')[0]).find('.mobile');
-      //    if (hehehehe.length) {
-      //      //var name = $(hehehehe).find('a').text();
-      //      var type = $(hehehehe).attr('class').substr(7);
-      //      var isExist = true;
-      //      $(done).each(function (index2, element2) {
-      //        if (element2.type === type) {
-      //          element2.sum++;
-      //          isExist = false;
-      //          return false;
-      //        }
-      //      });
-      //      if (isExist) {
-      //        done.push({
-      //          type: type,
-      //          sum: 1
-      //        });
-      //      }
-      //    } else {
-      //      done[0].sum++;
-      //    }
-      //  });
-      //}
-    };
-    co(gen2).then(function () {
-      console.log(done);
-    }).catch(function (error) {
-      console.log("gen2 error: " + error);
-    });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  co(gen2).then(function () {
+    console.log(JSON.stringify(done, null, 2));
+    fs.writeFile(path.join(__dirname, 'log.log'), new Date() + '\n' + JSON.stringify(done, null, 2) + '\n');
   }).catch(function (error) {
-    console.log("gen1 error: " + error);
+    console.log("gen2 error: " + error);
   });
+}).catch(function (error) {
+  console.log("gen1 error: " + error);
 });
